49e565bb10d237b8d39a1724d9204053503ed513
nkhavrat <nkhavrat@mail.ru>
2026-08-18 16:56:48 +0400

    Добавление файлов

---
+++ lessen_1.py
@@ -0,0 +1,25 @@
+my_heigh = 168
+print(my_heigh)
+
+my_name = "Наталья"
+my_name = "Наталья Хаврат" 
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца?")
+print("Ваш любимчик -" + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+print_python() 
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
